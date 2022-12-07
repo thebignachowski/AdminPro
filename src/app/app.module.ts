0bcc3282be8ca80e366c02fa77e9723bb189ccda
NachoRodriguez <nacho.rodriguezbe@gmail.com>
2022-12-07 21:54:33 +0100

    frist app angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+
+import { AppComponent } from './app.component';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NopagefoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    SharedModule,
+    AuthModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-grafica1',
+  templateUrl: './grafica1.component.html',
+  styles: [],
+})
+export class Grafica1Component {
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [{ data: [350, 450, 100] }],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+}
